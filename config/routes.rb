07ca85c1e563ab76d5d38a9de566ed058bbedecd
Morgana <morgana.t.borges@gmail.com>
2022-07-04 17:22:45 -0300

    create api

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v2 do
+      resources :members
+      resources :bands
+    end
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
